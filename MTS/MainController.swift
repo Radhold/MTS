439e094c476ccd4a970491dc7a17d307e68daceb
Radhold <56448462+Radhold@users.noreply.github.com>
2022-01-28 01:29:04 +0300

    Initial Commit

---
+++ MTS/MainController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  MTS
+//
+//  Created by Yaroslav Fomenko on 27.01.2022.
+//
+import CoreLocation
+import UIKit
+
+class MainController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.view.backgroundColor = .white
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue: CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
+
+
+}
+
